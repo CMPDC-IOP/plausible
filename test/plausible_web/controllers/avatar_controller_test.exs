2125fa756962234888bdf828b7fc3a4805583020
zhutiannian <zhutiannian@gmail.com>
2026-09-01 11:26:57 +0800

    fix: generate user avatars locally

--- test/plausible_web/controllers/avatar_controller_test.exs
+++ test/plausible_web/controllers/avatar_controller_test.exs
@@ -1,36 +1,19 @@
 defmodule PlausibleWeb.AvatarControllerTest do
   use PlausibleWeb.ConnCase, async: true
 
-  import Mox
-  setup :verify_on_exit!
-
   setup {PlausibleWeb.FirstLaunchPlug.Test, :skip}
 
   describe "GET /avatar/:hash" do
-    test "proxies the request to gravatar", %{conn: conn} do
-      expect(
-        Plausible.HTTPClient.Mock,
-        :get,
-        fn "https://www.gravatar.com/avatar/myhash?s=150&d=identicon" ->
-          {:ok,
-           %Finch.Response{
-             status: 200,
-             body: "avatar response body",
-             headers: [
-               {"content-type", "image/png"},
-               {"cache-control", "max-age=300"},
-               {"expires", "soon"}
-             ]
-           }}
-        end
-      )
-
+    test "returns a deterministic local avatar", %{conn: conn} do
       conn = get(conn, "/avatar/myhash")
 
-      assert response(conn, 200) =~ "avatar response body"
-      assert {"content-type", "image/png"} in conn.resp_headers
-      assert {"cache-control", "max-age=300"} in conn.resp_headers
-      assert {"expires", "soon"} in conn.resp_headers
+      assert response(conn, 200) =~ "<svg"
+      assert get_resp_header(conn, "content-type") == ["image/svg+xml; charset=utf-8"]
+      assert get_resp_header(conn, "cache-control") == ["public, max-age=2592000"]
+
+      other_conn = get(recycle(conn), "/avatar/another-hash")
+
+      refute response(other_conn, 200) == response(conn, 200)
     end
   end
 end
